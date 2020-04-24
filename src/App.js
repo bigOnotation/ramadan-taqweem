4483962984e6f1e84a5f0f657723a0e513aec259
muhammad-najimov <najimovm@mail.ru>
2020-04-24 21:39:58 +0500

    Update copyright

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 			<Duas />
 			<footer>
 				<p className="ayah">ٱهْدِنَا ٱلصِّرَٰطَ ٱلْمُسْتَقِيمَ</p>
-				<a href="https://t.me/bigOnotation">&copy; Muhammad Najimov 2020</a>
+				<a href="https://t.me/bigOnotation">&copy; Big O Notation 2020</a>
 			</footer>
 		</div>
 	)
